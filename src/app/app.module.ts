67dbef3e5a4a54575c0042263f446d532f53db66
akhi <dakhilesh95@gmail.com>
2019-11-16 16:18:17 +0530

    try one more 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { EditComponent } from './edit/edit.component';
-import { CatagoryComponent } from './catagory/catagory.component';
-import { ContactComponent } from './contact/contact.component';
-import { NfComponent } from './nf/nf.component';
-
-import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    RegisterComponent,
-    EditComponent,
-    CatagoryComponent,
-    ContactComponent,
-    NfComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-    {path:'',component:HomeComponent},
-    {path:'home',component:HomeComponent},
-    {path:'contact',component:ContactComponent},
-    {path:'catagory',component:CatagoryComponent},
-    {path:'edit',component:EditComponent},
-    {path:'register',component:RegisterComponent},
-    {path:'**',component:NfComponent},
-    ])
+    BrowserModule
   ],
-  providers: [DataService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
